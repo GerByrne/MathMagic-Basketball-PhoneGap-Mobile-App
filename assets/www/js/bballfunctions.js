52dbed19d7f3b72bbe4d00a6e314b1b6d5dea9da
Gerb68 <gerb68@hotmail.com>
2015-02-04 22:48:56 +0000

    added PhoneGap

---
+++ assets/www/js/bballfunctions.js
@@ -0,0 +1,129 @@
+﻿var nscore;
+  var draggedBballRemoval; 					
+
+  window.setInterval (BlinkIt, 250);
+  var color = "#ffffff";
+  function BlinkIt () {
+    var blink = document.getElementById ("blink");
+    color = (color == "#0000FF")? "#ffff00" : "#0000FF";
+    blink.style.color = color;    
+  }
+  
+  function ballSelection(clicked_bball_id)
+  {
+  document.getElementById("drop_zone").innerHTML="";			//empties the dropzone when wrong answer i.e. only 1 ball ata time in the net
+  
+  draggedBballRemoval = document.getElementById(clicked_bball_id);  
+  var dunk = document.getElementById("drop_zone");
+  dunk.appendChild(document.getElementById(clicked_bball_id).cloneNode(true)).classList.add('wobble-vertical');     //the ball sent to the dropzone (net)
+  dunk.style.opacity = '0.8';  
+  				
+  draggedBballRemoval.style.opacity = '0';   // remove bball   The best option - keeps the shape of the <td> tag. 
+  
+   // Find the object bball1 and only bball1 in the target drop_zone
+    if($('#drop_zone').find('#bball1').size() == 1 && randomBall  == 1 && level == 1) //.length detects the number of elements currently matched. The .size() method will return the same value.
+    {
+     correctAnswer(level);      
+    }
+  
+    /*if the object bball2 and only bball2 is in the target drop_zone draggedData equals 2 */
+    else if($('#drop_zone').find('#bball2').size() == 1 && randomBall == 2 && level == 1) //.length detects the number of elements currently matched. The .size() method will return the same value.
+    {  
+    correctAnswer(level);   
+    }
+  
+    else if($('#drop_zone').find('#bball3').size() == 1 && randomBall == 3 && level == 1) 
+    { 
+    correctAnswer(level);   
+    }
+
+    else if($('#drop_zone').find('#bball4').size() == 1 && randomBall == 4 && level == 1) 
+    { 
+     correctAnswer(level);   
+    }
+    
+	/********************************** Normal ************************************************************/
+	else if($('#drop_zone').find('#bball1').size() == 1 && randomBall  == 1 && level == 2)
+    {
+    correctAnswer(level);      
+    }
+  
+    else if($('#drop_zone').find('#bball2').size() == 1 && randomBall == 2 && level == 2) 
+    {  
+    correctAnswer(level);   
+    }
+  
+    else if($('#drop_zone').find('#bball3').size() == 1 && randomBall == 3 && level == 2) 
+    {
+    correctAnswer(level);   
+    }
+
+    else if($('#drop_zone').find('#bball4').size() == 1 && randomBall == 4 && level == 2) 
+    { 
+    correctAnswer(level);   
+    }  
+        
+    /********************************** Difficult ************************************************************/
+	else if($('#drop_zone').find('#bball1').size() == 1 && randomBall  == 1 && level == 3)
+    {
+    correctAnswer(level);      
+    }
+  
+    else if($('#drop_zone').find('#bball2').size() == 1 && randomBall == 2 && level == 3) 
+    {  
+    correctAnswer(level);   
+    }
+  
+    else if($('#drop_zone').find('#bball3').size() == 1 && randomBall == 3 && level == 3) 
+    {
+    correctAnswer(level);   
+    }
+
+    else if($('#drop_zone').find('#bball4').size() == 1 && randomBall == 4 && level == 3) 
+    { 
+    correctAnswer(level);   
+    }  
+    /*********************************************************************************************************/  
+    
+    else {    
+    //document.getElementById("bball1").className = "shake";    
+    document.getElementById("notification_panel").style.fontSize="large";     
+    document.getElementById("notification_panel").innerHTML="Wrong Answer! <br> Try again";
+    document.getElementById("notification_panel").style.color="yellow"; 
+    document.getElementById("notification_panel").style.backgroundColor="transparent";     
+    document.getElementById("notification_panel").style.textShadow="-1px 0 red, 0 1px red, 1px 0 red, 0 -1px red, 4px 4px 7px red";  // , [x-offset] [y-offset] [blur] [color]       
+    }  
+}
+  
+function correctAnswer(levelnumber) {												  
+    document.getElementById("notification_panel").setAttribute("style", "color:#0000FF; font-size:36px; font-weight:bold;");
+    document.getElementById("notification_panel").innerHTML="<span id='blink'>CORRECT</span>";    
+                              						    						  
+    setTimeout(function() {
+     document.getElementById("notification_panel").innerHTML="";
+     document.getElementById("drop_zone").innerHTML="";
+     var rand=Math.floor((Math.random()*4)+1);  //rand will be a random between 1 and 4     
+     
+      if (rand === 1 && levelnumber===1) { addEasy(); additionFunction(); }                                            
+      else if (rand === 2 && levelnumber===1) { subEasy(); subtractionFunction(); }
+      else if (rand === 3 && levelnumber===1) { multiEasy(); multiplicationFunction(); }
+      else if (rand === 4 && levelnumber===1) { divEasy(); divisionFunction(); }
+    
+      if (rand === 1 && levelnumber===2) { addNorm(); additionFunction(); }                                           
+      else if (rand === 2 && levelnumber===2) { subNorm(); subtractionFunction(); }
+      else if (rand === 3 && levelnumber===2) { multiNorm(); multiplicationFunction(); }
+      else if (rand === 4 && levelnumber===2) { divNorm(); divisionFunction(); }
+     
+      if (rand === 1 && levelnumber===3) { addDiff(); additionFunction(); }                                           
+      else if (rand === 2 && levelnumber===3) { subDiff(); subtractionFunction(); }
+      else if (rand === 3 && levelnumber===3) { multiDiff(); multiplicationFunction(); }
+      else if (rand === 4 && levelnumber===3) { divDiff(); divisionFunction(); }
+    
+     bball1.style.opacity = '1';
+     bball2.style.opacity = '1';
+     bball3.style.opacity = '1';
+     bball4.style.opacity = '1'; 
+     /******************************************/
+    }, 800);  //end of setTimeout                                    
+    updateScore();      
+  }// end of correctAnswer function    
---
+++ assets/www/js/timer.js
@@ -0,0 +1,105 @@
+﻿count = 600;   
+var t = null;
+var timespan;
+var currentScoreTop = 0;
+var currentScoreStore = 0;
+var storedEntries = [];
+var highScoreList;
+var gamescorelist;
+
+function init(){ 
+     timespan = document.getElementById('timespanId');     
+     countDownDisplay();
+     scoresListDisplay();
+}
+
+function countDownDisplay() {	                            
+    if (count === 600) {																
+     timespan.innerHTML = "";      
+    }
+    else {
+     timespan.innerHTML = ~~(count/10)+" secs";   
+    }  
+}
+   
+function startTimer(){ 							
+    if (t==null && count>0){     
+     countdown();    
+    }
+}
+
+function updateScore() {
+    document.getElementById("inGameScoreTop").innerHTML="Score: " + (currentScoreTop+=10);
+    document.getElementById("inGameScoreTable").innerHTML=(currentScoreStore+=10)+" Scored";     
+}
+
+function countdown() {
+    countDownDisplay();       
+    if (count == 0) {      
+      if(currentScoreStore<10) {    
+      document.getElementById("inGameScoreTable").innerHTML="No Score"; 
+      alert('Time is up');        
+     }      
+    
+	else { 
+	  var username;      
+	  var inputName=prompt("\t\tTime up\n\tYou Scored "+currentScoreStore+"\nEnter Your Name Here \n\t",""); 
+				
+      if (inputName!=null) {
+		 username=inputName;		          
+		 if(inputName==="null" || inputName==="")
+		 username="No name"; 
+		
+	     ////////////////////////////////////// High Scores/ARRAY Logic/////////////////////////////////////////////////////////  
+	     storedEntries = JSON.parse(localStorage.getItem("localstoragekey"));						//retrieve the original array as string	//convert/parse the original array into a new array
+		 if(storedEntries == null) storedEntries = [];		
+		   storedEntries.push(currentScoreStore+" "+username);   	   									    
+		   storedEntries = storedEntries.sort(function(a,b){return parseInt(b)-parseInt(a)});
+		   localStorage.setItem("localstoragekey", JSON.stringify(storedEntries));               	//store the original array as a string with the key id of localstoragekey
+		   storedEntries.length=10;   	   
+		 //////////////////////////////////////////////////
+	  }//end if (inputName!=null)
+	 } //end if else currentScoreStore	 
+	resetScoreTable();  
+	} //end if count
+    else {   
+      count--;      
+      t = setTimeout("countdown()", 100);   	
+    } //end else  
+}
+
+function scoresListDisplay(){
+        /***************************************************************************************/
+		/*  get the value of JSON.parse and, if it is not null, 0, NaN, undefined, false or '' */
+		/*  initialise storedEntries with it; otherwise, initialise storedEntries with [].     */
+		/***************************************************************************************/
+		storedEntries = JSON.parse(localStorage.getItem("localstoragekey")) || [];
+		highScoreList = gameTable.querySelector("section#gameTable ol.high-scores"); 
+	    for(var i = 0; i < 10; i++){
+          var st = storedEntries[i];                                   
+          gamescorelist = document.createElement('li');
+          gamescorelist.innerHTML = (typeof(st) != "undefined" ? st : "--" );  // if (typeof(st) != "undefined") is true.. then gamescorelist.innerHTML is given the value of the a variable st , otherwise gamescorelist.innerHTML is given the value of the ""
+          highScoreList.appendChild(gamescorelist);   //append the child li (with the string values in the array) of the highScoreList orderd list
+        }        
+}
+
+function clearLocalStorage() {
+localStorage.clear();
+resetScoreTable();
+}
+
+function resetScoreTable() {
+document.getElementById("main").innerHTML="";  //remove main content
+document.getElementById("bb").innerHTML = "";
+scoresListDisplay();
+document.getElementById("gameTable").className = "high-scores";
+document.getElementById("header").style.display= "none"; 
+document.getElementById("footer").style.display= "none";
+document.getElementById("scoresTable").style.display= "block";
+}  
+
+
+window.onload = init; 
+
+		
+
